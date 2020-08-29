7a5b36d4d919c37e4c42159cc02c7bcfde110dd2
Rodolfo Perez Leal <rodolpotter@hotmail.com>
2020-08-29 15:23:33 -0500

    2D Movement
    Basic Implementation of a character controler to do left and right movement of the character

---
+++ Kirbys Clone/Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class CharacterController2D : MonoBehaviour
+{
+    [Range(0, .3f)] [SerializeField] private float smooth = 0.05f;
+    private Rigidbody rb;
+    private Vector3 velocity = Vector3.zero;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody>();
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move(float move){
+        //Check if the player have control on the character
+        //----
+
+        //Move the character to find a target velocity
+        Vector3 targetVelocity = new Vector2(move * 10f, rb.velocity.y);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, smooth);
+        
+        //Flip character
+        // +++++
+        
+    }
+}
---
+++ Kirbys Clone/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public CharacterController2D controller;
+    private Vector3 movement;
+    private float xMovement = 0.0f; 
+
+    //call every frame
+    void Update()
+    {
+        //2D movement
+        xMovement = Input.GetAxis("Horizontal") * speed;
+
+    }
+        
+    
+
+    private void FixedUpdate() {
+        controller.Move(xMovement);
+    }
+
+}
