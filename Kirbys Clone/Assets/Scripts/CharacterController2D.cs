ad3920ec25af2f0fc8f6516366de2ac2a03dc352
Rodolfo Perez Leal <rodolpotter@hotmail.com>
2020-09-20 22:19:45 -0500

    Float
    Added Primitive float mechanic and crouching to the character controller script

--- Kirbys Clone/Assets/Scripts/CharacterController2D.cs
+++ Kirbys Clone/Assets/Scripts/CharacterController2D.cs
@@ -1,21 +1,36 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
+using System.Collections;
 
 public class CharacterController2D : MonoBehaviour
 {
     [Range(0, .5f)] [SerializeField] private float smooth = 0.05f;
     [SerializeField] private Transform groundCheck;
-    [SerializeField] private float jumpForce = 40f;
+    [SerializeField] private Transform ceilCheck;
+    [SerializeField] private float maxJump = 7000f;
+    [SerializeField] private float jumpForce = 7000f;
+    [SerializeField] private float gravity = -20f;
     [SerializeField] private LayerMask isGround;
+    [SerializeField] private float factor = 10f;
+    [Range(0,1)][SerializeField] private float crouchSpeed = 0.8f;
+    [Range(0,1)][SerializeField] private float flySpeed = 0.4f;
+    
+    [SerializeField] private Collider characterCollider;
+    
     private Rigidbody rb;
-    public bool isOnGround;    
+    private bool isOnGround;    
     private Vector3 velocity = Vector3.zero;
+    [SerializeField] private bool jumpEnabled;
+    private bool isCrouch;
+
+    [SerializeField] private float culdown = 1f;
 
     const float groundRadius = 0.2f;
+    const float ceilRadius = 0.2f;
 
 
     private void Awake() {
-        rb = GetComponent<Rigidbody>();   
+        rb = GetComponent<Rigidbody>(); 
     }
 
 
@@ -23,6 +38,9 @@ public class CharacterController2D : MonoBehaviour
     {
         bool wasGrounded = isOnGround;
         isOnGround = false;
+       /* jumpEnabled = false;
+        isCrouch = false;
+        */
 
         //Check if the  player is on something that is ground
         Collider[] colliders = Physics.OverlapSphere(groundCheck.position, groundRadius, isGround);
@@ -31,29 +49,82 @@ public class CharacterController2D : MonoBehaviour
 			if (colliders[i].gameObject != gameObject)
 			{
 				isOnGround = true;
+                jumpEnabled = true;
+                jumpForce = maxJump;
 			}
 		}
     }
 
-    public void Move(float move, bool jump){
-        //Check if the player have control on the character
-        //----
+    public void Move(float move, bool jump, bool crouch){
+        Vector3 targetVelocity;
 
+        //Jump if is the character is in the ground and the player press the jump button 
+        if(jump && jumpEnabled){
 
-        //Jump if is the character is in the ground and the player press the jump button
-        if(jump){
+            //Slowly decrece Jump Force
+            if(jumpForce > 2000f){
+                jumpForce *= .8f;
+            }
+            else{
+                jumpForce = 2000f;
+            }
+            print(jumpForce);
+
             isOnGround = false;
+            jumpEnabled = false;
             rb.AddForce(new Vector3(0, jumpForce, 0));
+            StartCoroutine(Jump());
+        }//if is flying only gravity is a force
+        else if(!isOnGround){
+            rb.AddForce(new Vector3(0, gravity, 0));
+        }
+        
+
+        //check if can stand up
+        if(!crouch){
+            Collider[] collidersTop = Physics.OverlapSphere(ceilCheck.position, ceilRadius, isGround);
+            //if not stay crouch
+            //Debug.Log(collidersTop.Length);
+            if(collidersTop.Length > 0){
+                crouch = true;
+            }
+        }
+        //when is crouch modify the colider and the speed
+        if(crouch && isOnGround){
+            isCrouch = true;
+            //targetVelocity = new Vector2(move * (factor * 0.5f), rb.velocity.y);
+            move *= crouchSpeed;
+            if(characterCollider != null) characterCollider.enabled = false;
+        }else if(crouch && !isOnGround){
+            move *= 0.9f;
+        }
+        else{
+            if(characterCollider != null) characterCollider.enabled = true;
         }
 
-        //Move the character to find a target velocity
-        Vector3 targetVelocity = new Vector2(move * 10f, rb.velocity.y);
+        //if the player is on the air, he has a slower movement 
+        if(!isOnGround){
+            move *= flySpeed;
+        }
+        
+        //Move the character to find the target velocity 
+        targetVelocity = new Vector2(move * factor, rb.velocity.y);
+        
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, smooth);
 
-        
+          
         
         //Flip character
         // +++++
         
     }
+
+    IEnumerator Jump()
+    {
+        //print(Time.time);
+        yield return new WaitForSecondsRealtime(culdown);
+        jumpEnabled = true;
+        //print(Time.time);
+    }
+
 }
--- Kirbys Clone/Assets/Scripts/PlayerMovement.cs
+++ Kirbys Clone/Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 movement;
     private float xMovement = 0.0f; 
     private bool jump = false;
+    public bool crouch = false;
 
     //call every frame
     void Update()
@@ -16,13 +17,18 @@ public class PlayerMovement : MonoBehaviour
         if(Input.GetButtonDown("Jump")){
             jump = true;
         }
+        if(Input.GetButtonDown("Crouch")){
+            crouch =  !crouch;
+        }
+        
     }
         
     
 
     private void FixedUpdate() {
-        controller.Move(xMovement, jump);
+        controller.Move(xMovement, jump, crouch);
         jump = false;
+        
     }
 
 }
