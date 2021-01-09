6627e492f8cdf93af9c3ddb5fa7aa2ca62c7a1b6
Rodolfo Perez Leal <rodolpotter@hotmail.com>
2021-01-08 20:57:19 -0600

    Slide update
    Minor corrections to the slide mechanic

--- Kirbys Clone/Assets/Scripts/CharacterController2D.cs
+++ Kirbys Clone/Assets/Scripts/CharacterController2D.cs
@@ -10,6 +10,8 @@ public class CharacterController2D : MonoBehaviour
     [SerializeField] private float jumpForce = 7000f;
     [SerializeField] private float gravity = -20f;
     [SerializeField] private LayerMask isGround;
+    [SerializeField] private LayerMask isSlope;
+
     [SerializeField] private float factor = 10f;
     [Range(0,1)][SerializeField] private float crouchSpeed = 0.8f;
     [Range(0,1)][SerializeField] private float flySpeed = 0.4f;
@@ -18,14 +20,16 @@ public class CharacterController2D : MonoBehaviour
     [SerializeField] private float gravityFactor = 1.5f;
 
     private Rigidbody rb;
+    private Transform tr;
     private bool isOnGround;    
     private Vector3 velocity = Vector3.zero;
     [SerializeField] private bool jumpEnabled;
 
     [SerializeField] private bool slideEnabled;
     [SerializeField] private float smoothSlide = 0.5f;
+    [SerializeField] private float slideForce = 50f;
 
-    [SerializeField] private float culdown = 1f;
+   // [SerializeField] private float culdown = 1f;
 
     const float groundRadius = 0.2f;
     const float ceilRadius = 0.2f;
@@ -33,6 +37,8 @@ public class CharacterController2D : MonoBehaviour
 
     private void Awake() {
         rb = GetComponent<Rigidbody>(); 
+        tr = GetComponent<Transform>();
+        slideEnabled = true;
     }
 
 
@@ -41,23 +47,49 @@ public class CharacterController2D : MonoBehaviour
         bool wasGrounded = isOnGround;
         isOnGround = false;
         jumpEnabled = false;
-        slideEnabled = true;
         //isCrouch = false;
         
-
         //Check if the  player is on something that is ground
-        Collider[] colliders = Physics.OverlapSphere(groundCheck.position, groundRadius, isGround);
-        for (int i = 0; i < colliders.Length; i++)
-		{
-			if (colliders[i].gameObject != gameObject)
-			{
+        Collider[] collidersGround = Physics.OverlapSphere(groundCheck.position, groundRadius, isGround);
+        for (int i = 0; i < collidersGround.Length; i++){
+           // Debug.Log(collidersGround[i]);
+			if (collidersGround[i].gameObject != gameObject){
 				isOnGround = true;
                 jumpEnabled = true;
+       /*         Vector3 ground = collidersGround[i].gameObject.transform.position;
+                float dotProduct = Vector3.Dot(ground.normalized, transform.position.normalized);
+                Debug.Log(dotProduct);
+                if(dotProduct == 0.0f){
+                    Debug.Log("Perpendicular");
+                }*/
 			}
 		}
+
+        //Check if the  player is on something that is ground
+        Collider[] collidersSlope = Physics.OverlapSphere(groundCheck.position, groundRadius, isSlope);
+        if(collidersSlope.Length > 0){
+            Debug.Log("Slide weee!");
+            rb.mass = 1;
+           // Slide(2.5f, 90f);
+        }
+        else{
+            rb.mass = 30;
+        }
     }
 
+
+
     public void Move(float moveX, float moveZ, bool jump, bool slide /*bool crouch*/){
+        int dir;
+        if(moveX < 0){
+            dir = -1;
+        }
+        else if(moveX > 0)
+            dir = 1;
+        else
+        {
+            dir = 0;
+        }
         Vector3 targetVelocity;
        // Vector3 move = new Vector3(moveX, 0, moveZ);
         //Jump if is the character is in the ground and the player press the jump button 
@@ -76,19 +108,16 @@ public class CharacterController2D : MonoBehaviour
         
         //if is possible to slide
         if(slide && slideEnabled){
-            Transform parent = transform.parent;
-            //rotate the character
-            parent.rotation  = Quaternion.Euler(0,0,90f);  
-
-            //move the character forward
+            moveX = Slide(0.8f, -90f * dir) * dir;
+            moveZ = 0;
         }    
 
         //check if can stand up
      /*   if(!crouch){
-            Collider[] collidersTop = Physics.OverlapSphere(ceilCheck.position, ceilRadius, isGround);
+            Collider[] collidersGroundTop = Physics.OverlapSphere(ceilCheck.position, ceilRadius, isGround);
             //if not stay crouch
-            //Debug.Log(collidersTop.Length);
-            if(collidersTop.Length > 0){
+            //Debug.Log(collidersGroundTop.Length);
+            if(collidersGroundTop.Length > 0){
                 crouch = true;
             }
         }
@@ -120,5 +149,23 @@ public class CharacterController2D : MonoBehaviour
                  
         //Flip character
         
+    }
+
+    private float Slide(float time, float angle){
+        Transform parent = transform.parent;
+        //rotate the character
+        transform.rotation  = Quaternion.Euler(0,0,angle);  
+        slideEnabled = false;
+        StartCoroutine(EndSlide(time));
+        //move the character forward
+        return slideForce;
+    }
+
+    //Coroutine to slide for 2.5 sec and then rise again
+    private IEnumerator EndSlide(float time){
+        slideEnabled = false;
+        yield return new WaitForSeconds(time);
+        transform.rotation  = Quaternion.Euler(0,0,0); 
+        slideEnabled = true;
     }
 }
